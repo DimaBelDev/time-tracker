e6903e2fb26f7e2e69ed39ae5cef5fb13ade21c9
salita_dima <salitadima1@gmail.com>
2024-11-01 17:02:37 +0100

    Init Commit

---
+++ time-tracker-app/src/main/java/com/rubaks/timetrackerapp/TimeTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.rubaks.timetrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTrackerAppApplication.class, args);
+    }
+
+}
